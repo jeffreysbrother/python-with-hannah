c168581010c30b72fecb12d35bdf3d3a34cafff2
James Cool <jeffreysbrother@gmail.com>
2016-08-13 13:44:43 -0700

    add some buttons to the UI

--- format.py
+++ format.py
@@ -5,7 +5,21 @@ root = Tk()
 
 #modify root window
 root.title("Format dat Shiz")
-root.geometry("200x100")
+root.geometry("400x200")
+
+app = Frame(root)
+app.grid()
+
+button1 = Button(app, text = "compute")
+button1.grid()
+
+button2 = Button(app)
+button2.grid()
+button2.configure(text = "pump it up")
+
+button3 = Button(app)
+button3.grid()
+button3["text"] = "add content"
 
 #kick off the event loop
 root.mainloop()
