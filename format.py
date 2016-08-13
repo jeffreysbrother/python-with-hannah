0438ca6b375a35051f4a1ae044358e87eae1773c
James Cool <jeffreysbrother@gmail.com>
2016-08-13 13:37:46 -0700

    make GUI thing for renaming stuffz

---
+++ format.py
@@ -0,0 +1,11 @@
+from Tkinter import *
+
+#create the window
+root = Tk()
+
+#modify root window
+root.title("Format dat Shiz")
+root.geometry("200x100")
+
+#kick off the event loop
+root.mainloop()
