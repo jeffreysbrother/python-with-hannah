217e441417cb910474c2ac6e8de5c762525f56a6
James Cool <jeffreysbrother@gmail.com>
2016-08-23 19:49:25 -0700

    add to function

--- review/instantiation.py
+++ review/instantiation.py
@@ -6,10 +6,12 @@
 class Mystuff:
     x = 13
     y = "yeah, bro."
-
+    def shout(self):
+        print("ANAL!!!")
 
 # instantiate a class
 things = Mystuff()
 
 # access an inherited value
 print things.x
+things.shout()
