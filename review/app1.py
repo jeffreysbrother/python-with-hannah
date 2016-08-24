13fe1fa957061eae68967a82ca40c8f720575dc3
James Cool <jeffreysbrother@gmail.com>
2016-08-23 19:15:27 -0700

    add explanation about import method in python

--- review/app1.py
+++ review/app1.py
@@ -5,9 +5,12 @@
 #
 # "from module import foo" -- less typing (no need to namespace), maybe harder to understand (context lost)
 #
+# in the link above there is also a discussion about 'from module import *' (not advised)
+#
 # still not clear as to whether this imports functions, classes, both, or whatever (or anything)
 
-from poopy import nonsense
+from poopy import catholic
 
+# only the catholic function is imported, so an attempt to use the nonsense function will cause the interpreter to throw an error
 catholic()
 nonsense()
--- review/poopy.py
+++ review/poopy.py
@@ -1,5 +1,5 @@
-def nonsense():
-    print("that is one big woman!")
-
 def catholic():
     print("feminist Mary, the creator of the World.")
+
+def nonsense():
+    print("that is one big woman!")
